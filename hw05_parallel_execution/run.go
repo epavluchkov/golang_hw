da9b3d17ba3cba2669db4540d637843a8eb2ee78
epavluchkov <e.pavluchkov@gmail.com>
2021-09-05 14:11:55 +0300

    HW5 2021-09-05 1

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,41 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	tCh := make(chan Task)
+	mu := sync.Mutex{}
+	errCnt := 0
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for t := range tCh {
+				err := t()
+				if err != nil {
+					mu.Lock()
+					errCnt++
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+
+	for _, t := range tasks {
+		mu.Lock()
+		if errCnt >= m && m != 0 {
+			mu.Unlock()
+			break
+		}
+		mu.Unlock()
+		tCh <- t
+	}
+	close(tCh)
+
+	wg.Wait()
+
+	if errCnt >= m && m != 0 {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
