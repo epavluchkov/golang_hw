984cd5532deb055f8d59eb50e135859486bf8577
epavluchkov <e.pavluchkov@gmail.com>
2021-09-09 14:37:33 +0300

    HW7 2021-09-09 1

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,14 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		Copy("testdata/input.txt", "tmp/out.txt", 8000, 0)
+		require.EqualError(t, ErrOffsetExceedsFileSize, "offset exceeds file size")
+	})
 }
