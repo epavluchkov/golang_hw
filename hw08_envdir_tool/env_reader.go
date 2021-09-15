21450b6eb26e88401523c08a91460de875893664
epavluchkov <e.pavluchkov@gmail.com>
2021-09-15 23:09:03 +0300

    HW8 2021-09-15 1

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +19,38 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment, len(files))
+
+	for _, fileInfo := range files {
+		if fileInfo.Size() == 0 {
+			env[fileInfo.Name()] = EnvValue{"", true}
+		} else {
+			file, err := os.Open(dir + "/" + fileInfo.Name())
+			if err != nil {
+				return nil, err
+			}
+
+			scan := bufio.NewScanner(file)
+			scan.Scan()
+			val := scan.Text()
+
+			if err := scan.Err(); err != nil {
+				return nil, err
+			}
+
+			val = strings.TrimRight(val, " \t")
+			val = string(bytes.Replace([]byte(val), []byte(string(rune(0x00))), []byte("\n"), 1))
+
+			env[fileInfo.Name()] = EnvValue{val, false}
+
+			file.Close()
+		}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for key, val := range env {
+		os.Unsetenv(key)
+		if !val.NeedRemove {
+			os.Setenv(key, val.Value)
+		}
+	}
+
+	com := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
+	com.Stdin = os.Stdin
+	com.Stdout = os.Stdout
+	com.Stderr = os.Stderr
+
+	if err := com.Run(); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	return 0
 }
